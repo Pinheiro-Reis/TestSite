188e575ef2c14e3c48d350ba2632876d5df4df23
João Victor <joaovictorpinheiro2007@gmail.com>
2025-02-13 22:40:54 -0300

    Add votes functionality

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
-from django.template import loader
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -13,21 +13,33 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     
     return render(request, "polls/detail.html", {
         "question": question,
     })
 
 def results(request, question_id):
-    response = f"You're looking the response to question {question_id}."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/results.html", {
+        "question": question,
+    })
 
 def vote(request, question_id):
-    c = Choice.objects.get(id=question_id)
-    c.votes += 1
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

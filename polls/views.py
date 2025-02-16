b08c54f2403f484237d6197d94dc269bedca71e2
João Victor <joaovictorpinheiro2007@gmail.com>
2025-02-16 00:11:20 -0300

    Update models, views and Add tests

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,29 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
+from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    
-    return render(request, "polls/detail.html", {
-        "question": question,
-    })
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-    return render(request, "polls/results.html", {
-        "question": question,
-    })
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -42,4 +41,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
+
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
